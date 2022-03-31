0e60bde966ef95ca72fb17f4e26d4dbd55dd8570
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-03-31 22:01:18 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return <div className="oui container-fluid m-0 p-0"></div>;
+}
