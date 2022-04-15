f3acb2b7fc6ccc0688494e0949b43fbaa0d0f4b6
Volosenco Andreea-Laura <91089388+imj31us4am50@users.noreply.github.com>
2022-04-15 08:40:31 +0300

    upd

--- maindonpage/Image.js
+++ maindonpage/Image.js
@@ -2,8 +2,8 @@ import ImageCSS from "/maindonpage/Image.css";
 
 export default function Image() {
   return (
-    <div className="container-fluid m-0 p-0" style={ImageCSS}>
-      <img src="imj31us4am50.github.io/imgs/b&w-cool-lion.jpg" />
+    <div className="container-fluid m-0 p-0 image-container" style={ImageCSS}>
+      <img src="imj31us4am50.github.io/imgs/cool-lion-bw.jpg" />
     </div>
   );
 }
